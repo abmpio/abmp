78bac7223ae505a1c0f57691a039ef14bf2fe925
jeseeqing <jeseeqing@gmail.com>
2025-12-26 21:31:20 +0800

    fix: remove rand.Seed func

--- pkg/utils/str/rand.go
+++ pkg/utils/str/rand.go
@@ -2,9 +2,7 @@ package str
 
 import (
 	"bytes"
-	"math"
 	"math/rand"
-	"time"
 )
 
 // 所有可用的十六进制字符，小写
@@ -16,7 +14,6 @@ var DecimalStrings []string = []string{"0", "1", "2", "3", "4", "5", "6", "7", "
 // 随机产生一个字符串
 func RandStrings(length int) string {
 	var buffer bytes.Buffer
-	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < length; i++ {
 		buffer.WriteString(HexStrings[rand.Intn(len(HexStrings)-1)])
 	}
@@ -29,7 +26,6 @@ func RandStringsIn(length int, inString []string) string {
 		return ""
 	}
 	var buffer bytes.Buffer
-	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < length; i++ {
 		buffer.WriteString(inString[rand.Intn(len(inString)-1)])
 	}
@@ -39,20 +35,9 @@ func RandStringsIn(length int, inString []string) string {
 // 随机产生一个指定长度的byte数组
 func RandByteArray(length int) []int8 {
 	datas := make([]int8, length)
-	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < length; i++ {
 		//随机生成-128到127之间的值
 		datas[i] = int8(rand.Intn(127) - 128)
 	}
 	return datas
-}
-
-// 在2个值之间随机一个值
-func RandInt32(minValue int, maxValue int) int {
-	rand.Seed(time.Now().UnixNano())
-	randValue := rand.Intn(maxValue)
-	if minValue < 0 {
-		return randValue - int(math.Abs(float64(minValue)))
-	}
-	return randValue
 }
